e590dc766492545b9ff21858ecb9ec4a22d75ed6
Leo Meyer <leomeyer@gmx.de>
2016-03-22 16:04:51 +0100

    DoorControl sketch library independence for newer Arduino IDEs

--- code/c/configs/Arduino/DoorControl/DoorControl/DoorControl.ino
+++ code/c/configs/Arduino/DoorControl/DoorControl/DoorControl.ino
@@ -27,83 +27,10 @@
  * - RFID access
  * - OPDI slave implementation
  *
- * Important! Copy the file DoorControl_secrets.inc to the installed OPDI library folder and
- * modify it to fit your desired settings!
+ * Copy the OPDI files to this folder (you can use the provided batch file for this)
+ * and restart the Arduino IDE, then compile.
  */
  
-/*
-// Arduino configuration specifications
-
-// set these values in opdi_configspecs.h in the Arduino library folder!
-
-#ifndef __OPDI_CONFIGSPECS_H
-#define __OPDI_CONFIGSPECS_H
-
-#define OPDI_IS_SLAVE	1
-#define OPDI_ENCODING_DEFAULT	OPDI_ENCODING_UTF8
-
-// Defines the maximum message length this slave can receive.
-// Consumes this amount of bytes in data and the same amount on the stack.
-#define OPDI_MESSAGE_BUFFER_SIZE		50
-
-// Defines the maximum message string length this slave can receive.
-// Consumes this amount of bytes times sizeof(char) in data and the same amount on the stack.
-// As the channel identifier and the checksum of a message typically consume a
-// maximum amount of nine bytes, this value may be OPDI_MESSAGE_BUFFER_SIZE - 9
-// on systems with only single-byte character sets.
-#define OPDI_MESSAGE_PAYLOAD_LENGTH	(OPDI_MESSAGE_BUFFER_SIZE - 9)
-
-// maximum permitted message parts
-#define OPDI_MAX_MESSAGE_PARTS	8
-
-// maximum length of master's name this device will accept
-#define OPDI_MASTER_NAME_LENGTH	1
-
-// maximum possible ports on this device
-#define OPDI_MAX_DEVICE_PORTS	7
-
-// define to conserve RAM and ROM
-//#define OPDI_NO_DIGITAL_PORTS
-
-// define to conserve RAM and ROM
-// Arduino compiler may crash or loop endlessly if this defined, so better keep them
-//#define OPDI_NO_ANALOG_PORTS
-
-// define to conserve RAM and ROM
-#define OPDI_NO_SELECT_PORTS
-
-// define to conserve RAM and ROM
-// #define OPDI_NO_DIAL_PORTS
-
-// maximum number of streaming ports
-// set to 0 to conserve both RAM and ROM
-#define OPDI_STREAMING_PORTS		0
-
-// Define to conserve memory
-#define OPDI_NO_ENCRYPTION
-
-// Define to conserve memory
-// #define OPDI_NO_AUTHENTICATION
-
-#define OPDI_HAS_MESSAGE_HANDLED
-
-// Defines for the OPDI implementation on Arduino
-
-// keep these numbers as low as possible to conserve memory
-#define MAX_PORTIDLENGTH		2
-#define MAX_PORTNAMELENGTH		5
-#define MAX_SLAVENAMELENGTH		12
-#define MAX_ENCODINGNAMELENGTH          7
-
-#define OPDI_MAX_PORT_INFO_MESSAGE	0
-
-#define OPDI_EXTENDED_INFO_LENGTH	16
-
-#define OPDI_EXTENDED_PROTOCOL          1
-
-#endif		// __OPDI_CONFIGSPECS_H
-
-*/ 
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
@@ -111,9 +38,9 @@
 #include "WProgram.h"
 #endif
 
-#include <opdi_constants.h>
+#include "opdi_constants.h"
 
-#include <DoorControl_secrets.inc>
+#include "DoorControl_secrets.h"
 
 #include <EEPROM.h>
 #include <SPI.h>
